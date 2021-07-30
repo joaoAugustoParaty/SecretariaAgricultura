c2b5fe502ea575cf4cc19e54246cbc5b315ded95
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-07-29 22:08:50 -0300

    Dia 5

--- js/main.js
+++ js/main.js
@@ -13,35 +13,25 @@ $(document).ready(function(){
             $(".navbar").removeClass("navbar-shrink")
         }
     })
-    /*--------------FEATURES CAROUSEL--------------*/
-    $('.features-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        autoplay:true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                
-            },
-            600:{
-                items:2,
-                
-            },
-            1000:{
-                items:3,
-                
-            }
-        }
-    })
-    /*------------------Page Scrolling- ScrollIt---------------*/
-    $.scrollIt({
-        topOffset: -50
-    });
-     /*-------------------Navbar Collapse-----------------------*/
-     $(".nav-link").on("click", function(){
-         $(".navbar-collapse").collapse("hide")
-     })
-
-     
 })
+// counter design
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration){
+        let obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        increment = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration /  range)),
+        timer = setInterval(() => {
+            current += increment;
+            obj.textContent = current;
+            if(current == end){
+                clearInterval(timer);
+            }
+        }, step);
+    }
+    counter("count1", 0, 100, 100);
+    counter("count2", 0, 100, 100);
+    counter("count3", 0, 200, 200);
+   
+ });
