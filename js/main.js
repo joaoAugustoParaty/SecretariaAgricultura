dc54014d4c495111d296ce698a3a3adbdd078e66
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-08-02 17:03:24 -0300

    dia 5

--- js/main.js
+++ js/main.js
@@ -35,3 +35,25 @@ document.addEventListener("DOMContentLoaded", () => {
     counter("count3", 0, 200, 200);
    
  });
+
+ /*--------------TESTIMONIALS CAROUSEL--------------*/
+ $('.noticia-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            
+        },
+        600:{
+            items:2,
+            
+        },
+        1000:{
+            items:3,
+            
+        }
+    }
+})
